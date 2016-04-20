7cd3a99d64e2db9ff7b4f873f8ea65c29543aee5
werwolf.by <werwolf.by@gmail.com>
2016-04-21 00:51:03 +0300

    feat(ui): allow to edit more settings for oil derrick head

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -19,17 +19,70 @@ class Main extends React.Component {
     super(props, context);
 
     this.handleOnChange = this.handleOnChange.bind(this);
+    let properties = {
+      'rotate': {title: 'Rotate', min: -180, max: 180, step: 1},
+      'cx': 'Center X',
+      'cy': 'Center Y',
+      'thickness': 'Thickness',
+      'left': 'Left',
+      'right': 'Right',
+      'headThickness': 'Head Thickness',
+      'headTop': 'Head Top',
+      'headBottom': 'Head Bottom',
+    };
+    this.sliders = Object.keys(properties).map(k => {
+      let value = properties[k];
+      let title = value;
+      let min = 0;
+      let max = 1;
+      let step = 0.01;
+
+      if (typeof value === 'object') {
+        title = value.title;
+        min = value.min;
+        max = value.max;
+        step = value.step;
+      }
+
+      return {
+        title: title,
+        min: min,
+        max: max,
+        step: step,
+        prop: k,
+        handler: (evt, value) => this.handleOnChange(value, k),
+      };
+    });
 
     this.state = {
-      rotate: 0,
+      rotate: -22,
+      cx: 0.47,
+      cy: 0.4,
+      thickness: 0.07,
+      left: 0.45,
+      right: 0.35,
+      headThickness: 0.11,
+      headTop: 0.16,
+      headBottom: 0.22,
     };
   }
 
-  handleOnChange(evt, value) {
-    this.setState(Object.assign({}, this.state, {rotate: value}));
+  handleOnChange(value, name) {
+    this.setState(Object.assign({}, this.state, {[name]: value}));
   }
 
   render() {
+    let sliders = this.sliders.map(s => {
+      let value = this.state[s.prop];
+
+      return (
+        <div key={s.prop}>
+          <div className="value">{s.title} ({value})</div>
+          <Slider className="slider" name="rotate" min={s.min} max={s.max} step={s.step} value={value} onChange={s.handler}/>
+        </div>
+        );
+      });
+
     return (
       <div className="container">
         <AppBar className="app-bar"
@@ -38,11 +91,10 @@ class Main extends React.Component {
         />
         <div className="container-content">
           <div className="content">
-            <Content rotate={this.state.rotate}/>
+            <Content {...this.state}/>
           </div>
-          <Paper rounded={false} className="settings" zDepth={3}>
-            <div className="value">Rotate ({this.state.rotate})</div>
-            <Slider className="slider" min={0} max={360} step={1} value={this.state.rotate} onChange={this.handleOnChange}/>
+          <Paper rounded={false} className="settings" style={{overflow: 'auto'}} zDepth={3}>
+            {sliders}
           </Paper>
         </div>
       </div>
