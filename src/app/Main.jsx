a06106a78df60cb0d680727e3a5d8e3276156b33
werwolf.by <werwolf.by@gmail.com>
2016-04-22 00:07:30 +0300

    feat(ui): add subheaders for different setting parts

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -19,42 +19,51 @@ class Main extends React.Component {
     super(props, context);
 
     this.handleOnChange = this.handleOnChange.bind(this);
-    let properties = {
+    this._mapSlider = this._mapSlider.bind(this);
+    this._subHeader = this._subHeader.bind(this);
+
+    let shaftProperties = {
       'rotate': {title: 'Rotate', min: -180, max: 180, step: 1},
       'cx': 'Center X',
       'cy': 'Center Y',
       'thickness': {title: 'Thickness', min: 0.01, max: 0.2, step: 0.001},
       'left': 'Left',
       'right': 'Right',
+    };
+    let headProperties = {
       'headThickness': {title: 'Head Thickness', min: 0.01, max: 0.2, step: 0.001},
       'headTop': 'Head Top',
       'headBottom': 'Head Bottom',
       'headQdx': 'Head Q dx',
       'headQdy': 'Head Q dy',
     };
-    this.sliders = Object.keys(properties).map(k => {
-      let value = properties[k];
-      let title = value;
-      let min = 0;
-      let max = 1;
-      let step = 0.01;
+    let createHandlers = (properties) => {
+      return Object.keys(properties).map(k => {
+        let value = properties[k];
+        let title = value;
+        let min = 0;
+        let max = 1;
+        let step = 0.01;
 
-      if (typeof value === 'object') {
-        title = value.title;
-        min = value.min;
-        max = value.max;
-        step = value.step;
-      }
+        if (typeof value === 'object') {
+          title = value.title;
+          min = value.min;
+          max = value.max;
+          step = value.step;
+        }
 
-      return {
-        title: title,
-        min: min,
-        max: max,
-        step: step,
-        prop: k,
-        handler: (evt, value) => this.handleOnChange(value, k),
-      };
-    });
+        return {
+          title: title,
+          min: min,
+          max: max,
+          step: step,
+          prop: k,
+          handler: (evt, value) => this.handleOnChange(value, k),
+        };
+      });
+    };
+    this.shaftSliders = createHandlers(shaftProperties);
+    this.headSliders = createHandlers(headProperties);
 
     this.state = {
       rotate: -22,
@@ -68,6 +77,13 @@ class Main extends React.Component {
       headBottom: 0.22,
       headQdx: 0.32,
       headQdy: 0.18,
+      muiTheme: context.muiTheme || getMuiTheme(),
+    };
+  }
+
+  getChildContext() {
+    return {
+      muiTheme: muiTheme,
     };
   }
 
@@ -75,17 +91,34 @@ class Main extends React.Component {
     this.setState(Object.assign({}, this.state, {[name]: value}));
   }
 
-  render() {
-    let sliders = this.sliders.map(s => {
-      let value = this.state[s.prop];
+  _mapSlider(s) {
+    let value = this.state[s.prop];
 
-      return (
-        <div key={s.prop}>
-          <div className="value">{s.title} ({value})</div>
-          <Slider className="slider" name="rotate" style={{marginTop: 7, marginBottom: 7}} min={s.min} max={s.max} step={s.step} value={value} onChange={s.handler}/>
-        </div>
-        );
-      });
+    return (
+      <div key={s.prop}>
+        <div className="value">{s.title} ({value})</div>
+        <Slider className="slider" name="rotate" style={{marginTop: 7, marginBottom: 7}} min={s.min} max={s.max} step={s.step} value={value} onChange={s.handler}/>
+      </div>
+      );
+  }
+
+  _subHeader(name) {
+    const muiTheme = this.state.muiTheme;
+    let themeVariables = muiTheme.appBar;
+
+    return (<div className="value" style={{backgroundColor: themeVariables.color, color: themeVariables.textColor, padding: 5}}>{name}</div>);
+  }
+
+  render() {
+    let shaftSliders = this.shaftSliders.map(this._mapSlider);
+    let headSliders = this.headSliders.map(this._mapSlider);
+
+    let sliders = [
+      this._subHeader("Shaft"),
+      ...shaftSliders,
+      this._subHeader("Head"),
+      ...headSliders,
+    ];
 
     return (
       <div className="container">
@@ -105,5 +138,9 @@ class Main extends React.Component {
     );
   }
 }
+
+Main.childContextTypes = {
+  muiTheme: React.PropTypes.object,
+};
 
 export default themeDecorator(muiTheme)(Main);
