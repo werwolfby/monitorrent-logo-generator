df6bbdf7a09f5e6cc3508cee1a44584b9cfb9b8c
werwolf.by <werwolf.by@gmail.com>
2016-04-28 21:31:58 +0300

    refactor(Settings): reorganize code to simplify add new features to settings

--- src/app/Settings.jsx
+++ src/app/Settings.jsx
@@ -11,101 +11,109 @@ import MenuItem from 'material-ui/lib/menus/menu-item';
 import getMuiTheme from 'material-ui/lib/styles/getMuiTheme';
 
 class Settings extends React.Component {
-  constructor(props, context) {
-    super(props, context);
+    constructor(props, context) {
+        super(props, context);
 
-    this.handleOnChange = this.handleOnChange.bind(this);
-    this._mapSlider = this._mapSlider.bind(this);
-    this._subHeader = this._subHeader.bind(this);
-    this._onChange = props.onChange;
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this._mapSlider = this._mapControl.bind(this);
+        this._subHeader = this._subHeader.bind(this);
+        this._onChange = props.onChange;
 
-    const properties = props.properties;
-    const createHandlers = (properties) => {
-      return Object.keys(properties).map(k => {
+        const properties = props.properties;
+        const createHandlers = (properties) => {
+            return Object.keys(properties).map(prop => this._createPropertySettings(prop, properties[prop]));
+        };
+        this.sliders = Object.keys(properties).map(h => ({ subheader: h, properties: createHandlers(properties[h]) }));
+
+        this.state = Object.assign({}, { values: props.values }, { muiTheme: context.muiTheme || getMuiTheme() });
+    }
+
+    handleOnChange(value, name) {
+        let values = Object.assign({}, this.state.values, { [name]: value });
+        this.setState({ values }, () => this._onChange(this.state.values));
+    }
+
+    _createPropertySettings(prop, value) {
+        function createSelect(value) {
+            return {
+                type: 'select',
+                values: value.values,
+            };
+        }
+
+        function createSlider(value) {
+            return {
+                type: 'slider',
+                min: value.min || 0,
+                max: value.max || 1,
+                step: value.step || 0.01,
+            };
+        }
+
         let ext = null;
-        let value = properties[k];
 
-        if (typeof value === 'object') {
-          if (Array.isArray(value.values)) {
-            ext = {
-              title: value.title,
-              values: value.values,
+        if (typeof value === 'string') {
+            value = {
+                title: value,
             };
-          }
-          else {
-            ext = {
-              title: value.title,
-              min: value.min,
-              max: value.max,
-              step: value.step,
-            };
-          }
-        } else {
-          ext = {
-            title: value,
-            min: 0,
-            max: 1,
-            step: 0.01,
-          };
+        }
+
+        if (Array.isArray(value.values)) {
+            ext = createSelect(value);
+        }
+        else {
+            ext = createSlider(value);
         }
 
         return Object.assign({
-            prop: k,
+            title: value.title,
+            prop: prop,
             handler: (evt, value) => this.handleOnChange(value, k),
-          }, ext);
-      });
-    };
-    this.sliders = Object.keys(properties).map(h => ({subheader: h, properties: createHandlers(properties[h])}));
-
-    this.state = Object.assign({}, {values: props.values}, {muiTheme: context.muiTheme || getMuiTheme()});
-  }
-
-  handleOnChange(value, name) {
-    let values = Object.assign({}, this.state.values, {[name]: value});
-    this.setState({values}, () => this._onChange(this.state.values));
-  }
-
-  _mapSlider(s) {
-    let value = this.state.values[s.prop];
-    let control = null;
-    if (s.values) {
-      let values = s.values.map(v => <MenuItem key={v.value} value={v.value} primaryText={v.title}/>)
-
-      return (
-        <div key={s.prop} style={{margin: 7}}>
-          <SelectField fullWidth={true} value={value} onChange={s.handler}>
-            {values}
-          </SelectField>
-        </div>
-      );
-    } else {
-      return (
-        <div key={s.prop}>
-          <div className="value">{s.title} ({value})</div>
-          <Slider className="slider" name="rotate" style={{marginTop: 7, marginBottom: 7}} min={s.min} max={s.max} step={s.step} value={value} onChange={s.handler}/>
-        </div>
-      );
-    }
-  }
-
-  _subHeader(name) {
-    const muiTheme = this.state.muiTheme;
-    let themeVariables = muiTheme.appBar;
-
-    return (<div className="value" key={name} style={{backgroundColor: themeVariables.color, color: themeVariables.textColor, padding: 5}}>{name}</div>);
-  }
-
-  render() {
-    let sliders;
-    if (this.sliders.length > 1) {
-      let mappedSliders = this.sliders.map(s => [this._subHeader(s.subheader), ...s.properties.map(this._mapSlider)]);
-      sliders = Array.prototype.concat(...mappedSliders);
-    } else {
-      sliders = this.sliders[0].properties.map(this._mapSlider);
+        }, ext);
     }
 
-    return (<div>{sliders}</div>);
-  }
+    _mapControl(s) {
+        let value = this.state.values[s.prop];
+        let control = null;
+        switch (s.type) {
+            case 'select':
+                let values = s.values.map(v => <MenuItem key={v.value} value={v.value} primaryText={v.title}/>)
+
+                return (
+                    <div key={s.prop} style={{ margin: 7 }}>
+                        <SelectField fullWidth={true} value={value} onChange={s.handler}>
+                            {values}
+                        </SelectField>
+                    </div>
+                );
+            case 'slider':
+                return (
+                    <div key={s.prop}>
+                        <div className="value">{s.title} ({value}) </div>
+                        <Slider className="slider" name="rotate" style={{ marginTop: 7, marginBottom: 7 }} min={s.min} max={s.max} step={s.step} value={value} onChange={s.handler}/>
+                    </div>
+                );
+        }
+    }
+
+    _subHeader(name) {
+        const muiTheme = this.state.muiTheme;
+        let themeVariables = muiTheme.appBar;
+
+        return (<div className="value" key={name} style={{ backgroundColor: themeVariables.color, color: themeVariables.textColor, padding: 5 }}>{name}</div>);
+    }
+
+    render() {
+        let controls;
+        if (this.sliders.length > 1) {
+            let mappedControls = this.sliders.map(s => [this._subHeader(s.subheader), ...s.properties.map(prop => this._mapControl(prop))]);
+            controls = Array.prototype.concat(...mappedControls);
+        } else {
+            controls = this.sliders[0].properties.map(prop => this._mapControl(prop));
+        }
+
+        return (<div>{controls}</div>);
+    }
 }
 
 export default Settings;
